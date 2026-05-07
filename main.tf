8d2d894cc2b422c717d2e7ca3616f613d1021c09
mysak7 <michal.burdik@proton.me>
2026-05-07 20:51:36 +0200

    Add CNAME and TXT records for penny.mysak.fun

--- main.tf
+++ main.tf
@@ -21,6 +21,24 @@ resource "azurerm_dns_zone" "mysak_fun" {
   resource_group_name = azurerm_resource_group.dns.name
 }
 
+resource "azurerm_dns_cname_record" "penny" {
+  name                = "penny"
+  zone_name           = azurerm_dns_zone.mysak_fun.name
+  resource_group_name = azurerm_resource_group.dns.name
+  ttl                 = 300
+  record              = "ca-prd-eus-penny.thankfulsand-fdb51b76.eastus.azurecontainerapps.io"
+}
+
+resource "azurerm_dns_txt_record" "penny_verification" {
+  name                = "asuid.penny"
+  zone_name           = azurerm_dns_zone.mysak_fun.name
+  resource_group_name = azurerm_resource_group.dns.name
+  ttl                 = 300
+  record {
+    value = "24C4FD8D3A8507E43D386A411379665BC15579939C271A26E15AE5643A8A540A"
+  }
+}
+
 output "nameservers" {
   value       = azurerm_dns_zone.mysak_fun.name_servers
   description = "Nameservery ke zkopírování do WEDOS"
